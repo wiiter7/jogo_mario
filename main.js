0db64fbb42d5603ddd054c1493bf992516fc8027
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-06-09 07:18:54 -0300

    s

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const mario = document.querySelector(".mario")
 const pipe  = document.querySelector(".pipe")
 
-const jump = () =>
+const jump = () => {
+    mario.classList.add("jump");
+    
+    setTimeout(())
+}
