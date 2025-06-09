3af1c4ea0079964bd42a5cc1754069d159af00c1
wiiter7 <arthur.carneiro.santos@escola.pr.gov.br>
2025-06-09 07:40:22 -0300

    s

--- main.js
+++ main.js
@@ -3,6 +3,24 @@ const pipe  = document.querySelector(".pipe")
 
 const jump = () => {
     mario.classList.add("jump");
-    
-    setTimeout(())
+
+    setTimeout(() =>{
+        mario.classList.remove("jump");
+    }
+    ,500);
 }
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","")
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.Style.animation = "none";
+        pipe.Style.left = `${pipePosition}px`;
+
+        mario.Style.animation = "none";
+        mario.Style.left = `${marioPosition}px`;
+    }
+})
+document.addEventListener("keydown",jump);
+
